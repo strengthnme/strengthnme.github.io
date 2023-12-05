15c0a0c8f5f8936a5025b746df3738ae5e1802d7
strengthnme <152830130+strengthnme@users.noreply.github.com>
2023-12-04 21:40:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,13 @@
-let slideIndexOne = 1;
-    let slideIndexTwo = 1;
-
-    showSlidesOne(slideIndexOne);
-    showSlidesTwo(slideIndexTwo);
-
-    function showSlidesOne(index) {
-        const slides = document.querySelectorAll('.carousel-one-item');
-        if (index < 1) {
-            slideIndexOne = slides.length;
-        }
-        if (index > slides.length) {
-            slideIndexOne = 1;
-        }
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-        }
-        slides[slideIndexOne - 1].style.display = 'block';
-    }
-
-    function showSlidesTwo(index) {
-        const slides = document.querySelectorAll('.carousel-two-item');
-        if (index < 1) {
-            slideIndexTwo = slides.length;
-        }
-        if (index > slides.length) {
-            slideIndexTwo = 1;
-        }
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-        }
-        slides[slideIndexTwo - 1].style.display = 'block';
-    }
-
-    function changeSlideOne(n) {
-        showSlidesOne((slideIndexOne += n));
-    }
-
-    function changeSlideTwo(n) {
-        showSlidesTwo((slideIndexTwo += n));
-    }
-
-    document.querySelector(".carousel-container-one .back").addEventListener("click", function () {
-        changeSlideOne(-1);
-    });
-
-    document.querySelector(".carousel-container-one .forward").addEventListener("click", function () {
-        changeSlideOne(1);
-    });
-
-    document.querySelector(".carousel-container-two .prev").addEventListener("click", function () {
-        changeSlideTwo(-1);
-    });
-
-    document.querySelector(".carousel-container-two .next").addEventListener("click", function () {
-        changeSlideTwo(1);
-    });
+const slide = document.querySelector(".slider");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+btn1.onclick = function () {
+  slide.style.transform = "translateX(0px)";
+};
+btn2.onclick = function () {
+  slide.style.transform = "translateX(-100%)";
+};
+btn3.onclick = function () {
+  slide.style.transform = "translateX(-200%)";
+};
